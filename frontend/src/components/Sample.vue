c1f5729d54d334dfaee6ba4f31d4fe237235722d
auwau22visg <auwau22visg@gmail.com>
2021-10-25 08:48:40 +0900

    3dオブジェクトを表示させる

--- frontend/src/components/Sample.vue
+++ frontend/src/components/Sample.vue
@@ -16,31 +16,45 @@ export type Canvas = HTMLCanvasElement;
   },
 })
 export default class Sample extends Vue {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#myCanvas") as Canvas,
-  });
-  axes = new THREE.AxesHelper(20);
-
-  cubeGeo = new THREE.BoxGeometry(4, 4, 4);
-  cubeMat = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true,
-  });
-  cube = new THREE.Mesh(this.cubeGeo, this.cubeMat);
-
   mounted(): void {
-    this.renderer.setClearColor(new THREE.Color(0xeeeeee));
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.scene.add(this.axes);
-    this.scene.add(this.cube);
-    this.renderer.render(this.scene, this.camera);
+    let scene = new THREE.Scene();
+
+    let camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    let axes = new THREE.AxesHelper(20);
+
+    let cubeGeo = new THREE.BoxGeometry(4, 4, 4);
+    let cubeMat = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true,
+    });
+    let cube = new THREE.Mesh(cubeGeo, cubeMat);
+
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+
+    let renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#myCanvas") as Canvas,
+    });
+
+    renderer.setClearColor(new THREE.Color(0xeeeeee));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    scene.add(axes);
+    scene.add(cube);
+
+    renderer.render(scene, camera);
+
   }
 }
 </script>
